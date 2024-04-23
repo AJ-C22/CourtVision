b960f595428fda42c0515360b39ab2bd0af21e69
BenjaminNamayandev <benjaminnamayandeh@gmail.com>
2024-04-23 16:04:35 -0400

    Changed the detection to async

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
+import asyncio
 import cv2
 import base64
 import numpy as np
-import requests
+import httpx
 import time
 
-
 ROBOFLOW_SIZE = 416
-
 # Construct the Roboflow Infer URL
 # (if running locally replace https://detect.roboflow.com/ with eg http://127.0.0.1:9001/)
 upload_url = "".join([
@@ -22,7 +21,8 @@ upload_url = "".join([
 video = cv2.VideoCapture(0)
 
 # Infer via the Roboflow Infer API and return the result
-def infer():
+# Takes an httpx.AsyncClient as a parameter
+async def infer(requests):
     # Get the current image from the webcam
     ret, img = video.read()
 
@@ -36,32 +36,52 @@ def infer():
     img_str = base64.b64encode(buffer)
 
     # Get prediction from Roboflow Infer API
-    resp = requests.post(upload_url, data=img_str, headers={
+    resp = await requests.post(upload_url, data=img_str, headers={
         "Content-Type": "application/x-www-form-urlencoded"
-    }, stream=True).raw
+    })
 
     # Parse result image
-    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    image = np.asarray(bytearray(resp.content), dtype="uint8")
     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
 
     return image
 
-# Main loop; infers sequentially until you press "q"
-while 1:
-    # On "q" keypress, exit
-    if(cv2.waitKey(1) == ord('q')):
-        break
+# Main loop; infers at FRAMERATE frames per second until you press "q"
+async def main():
+    # Initialize
+    last_frame = time.time()
 
-    # Capture start time to calculate fps
-    start = time.time()
+    # Initialize a buffer of images
+    futures = []
 
-    # Synchronously get a prediction from the Roboflow Infer API
-    image = infer()
-    # And display the inference results
-    cv2.imshow('image', image)
+    async with httpx.AsyncClient() as requests:
+        while 1:
+            # On "q" keypress, exit
+            if(cv2.waitKey(1) == ord('q')):
+                break
 
-    # Print frames per second
-    print(round(1/(time.time()-start)), " fps")
+            # Throttle to FRAMERATE fps and print actual frames per second achieved
+            elapsed = time.time() - last_frame
+            await asyncio.sleep(max(0, 1/24 - elapsed))
+            print((1/(time.time()-last_frame)), " fps")
+            last_frame = time.time()
+
+            # Enqueue the inference request and safe it to our buffer
+            task = asyncio.create_task(infer(requests))
+            futures.append(task)
+
+            # Wait until our buffer is big enough before we start displaying results
+            if len(futures) < 12:
+                continue
+
+            # Remove the first image from our buffer
+            # wait for it to finish loading (if necessary)
+            image = await futures.pop(0)
+            # And display the inference results
+            cv2.imshow('image', image)
+
+# Run our main loop
+asyncio.run(main())
 
 # Release resources when finished
 video.release()

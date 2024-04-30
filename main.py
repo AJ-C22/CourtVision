6ef10e9aa4b6ad232d9d4398f1fcbb43420ccdd8
Ajai Chandi <ajaischandi@gmail.com>
2024-04-30 11:56:41 -0400

    Added predicting file

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
+import cv2
+
 from ultralytics import YOLO
-import multiprocessing
+import supervision as sv
+import numpy as np
 
-if __name__ == '__main__':
-    # Call freeze_support() to ensure proper handling of multiprocessing
-    multiprocessing.freeze_support()
 
-    # Load a model
-    model = YOLO("yolov8n.yaml")  
-    model.to('cuda')
+def main():
+    model = YOLO("best.pt")
 
-    # Use the model for training
-    results = model.train(data="config.yaml", epochs=225, batch=8, cache=False, workers=1)
+    box_annotator = sv.BoxAnnotator(
+        thickness=2,
+        text_thickness=1,
+        text_scale=0.5
+    )
+    
+    for result in model.track(source=0, show=True, stream=True, agnostic_nms=True):
+        
+        frame = result.orig_img
+        detections = sv.Detections.from_ultralytics(result)
+
+        labels = [
+            f"{tracker_id} {model.model.names[class_id]} {confidence:0.2f}"
+            for _, confidence, class_id, tracker_id
+            in detections
+        ]
+
+        frame = box_annotator.annotate(
+            scene=frame, 
+            detections=detections,
+            labels=labels
+        )
+
+        cv2.imshow("yolov8", frame)
+
+        if (cv2.waitKey(30) == 27):
+            break
+
+
+if __name__ == "__main__":
+    main()

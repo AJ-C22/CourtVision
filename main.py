ddcf5127381b5d0add62747adc1c82757f6200ab
Ajai Chandi <ajaischandi@gmail.com>
2024-04-29 21:53:46 -0400

    Trained model with 225 epochs

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from ultralytics import YOLO
+import multiprocessing
 
+if __name__ == '__main__':
+    # Call freeze_support() to ensure proper handling of multiprocessing
+    multiprocessing.freeze_support()
 
-# Load a model
-model = YOLO("yolov8n.yaml")  
+    # Load a model
+    model = YOLO("yolov8n.yaml")  
+    model.to('cuda')
 
-# Use the model for training
-results = model.train(data="config.yaml", epochs=11)
-
+    # Use the model for training
+    results = model.train(data="config.yaml", epochs=225, batch=8, cache=False, workers=1)

fa9dda2e390201fe9dd4c1248f9f27691421ac32
Ajai Chandi <ajaischandi@gmail.com>
2024-05-01 18:43:22 -0400

    Can now single out rim detection to make the two box system

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import cv2
-
 from ultralytics import YOLO
 import supervision as sv
 import numpy as np
-
 
 def main():
     model = YOLO("last.pt")
@@ -19,18 +17,20 @@ def main():
         frame = result.orig_img
         detections = sv.Detections.from_ultralytics(result)
         
-        #detections = detections[detections.class_id == 2]
+        for detection in detections:
+            class_id = detection[3]  
+            if class_id == 2:
+                print("RIM")
         
         frame = box_annotator.annotate(
             scene=frame, 
-            detections=[detections],
+            detections=detections,  # Pass detections directly without enclosing it in another list
         )
-        
+
         cv2.imshow("yolov8", frame)
 
-        if (cv2.waitKey(30) == 27):
+        if cv2.waitKey(30) == 27:
             break
-
 
 if __name__ == "__main__":
     main()

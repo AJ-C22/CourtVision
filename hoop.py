a26eaf3211c715934ad722b7870539ad13e3ed17
BenjaminNamayandev <benjaminnamayandeh@gmail.com>
2024-06-03 15:38:54 -0400

    Tried Implementing DeepSort

--- hoop.py
+++ hoop.py
@@ -17,16 +17,14 @@ class Shot:
         self.goal_count = 0
         self.ball_in_top_box = False
         self.ball_positions = []  # List to store ball positions for smoothing
+        self.team2_ids = set()  # Set to store team 2 IDs
         self.team_colors = defaultdict(lambda: (255, 0, 0))  # Default color blue for team 1
         self.num_orange_buckets = 0  # Score for orange team
         self.num_blue_buckets = 0  # Score for blue team
         self.current_shooting_team = None  # Team currently shooting
         self.last_shooting_team = None  # Last known shooting team
         self.frame_skip = 2  # Process every nth frame
-
-        # Initialize DeepSORT with a max age to keep tracks for longer
-        self.tracker = DeepSort(max_age=50, n_init=3, nms_max_overlap=1.0, max_cosine_distance=0.2)
-        
+        self.tracker = DeepSort(max_age=30)  # Initialize DeepSORT
         self.run()
     
     def run(self):
@@ -54,7 +52,7 @@ class Shot:
 
             results = self.model(self.frame, stream=True)
             current_frame_dots = []  # Temporary list to hold dots for the current frame
-            detections = []  # List to hold detections for DeepSORT
+            detections = []
 
             for r in results:
                 boxes = r.boxes
@@ -71,47 +69,31 @@ class Shot:
                     current_class = self.class_names[cls]
 
                     # Draw rectangle
-                    if current_class == "ball" and conf > 0.35:
-                        cv2.rectangle(self.frame, (x1, y1), (x2, y2), (255, 165, 0), 2)
-                        ball_position = (cx, cy)
-                    
-                    elif current_class == "person" and conf > 0.7:
-                        detections.append(((x1, y1, x2 - x1, y2 - y1), conf, cls))  # For DeepSORT
+                    if conf > 0.4:  # Adjust the confidence threshold
+                        if current_class == "ball":
+                            cv2.rectangle(self.frame, (x1, y1), (x2, y2), (255, 165, 0), 2)
+                            ball_position = (cx, cy)
+                        
+                        elif current_class == "person":
+                            detections.append(([x1, y1, x2-x1, y2-y1], conf, 2))  # Add detection in [x, y, w, h] format
 
-                    elif current_class == "rim" and conf > 0.7:
-                        cv2.rectangle(self.frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
-                        rim_position = (cx, cy)
+                        elif current_class == "rim":
+                            cv2.rectangle(self.frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                            rim_position = (cx, cy)
 
-            # Update DeepSORT tracker with detections
+            # Update DeepSORT with detections
             tracks = self.tracker.update_tracks(detections, frame=self.frame)
 
-            centroids = []  # List to store centroids of detected people
-            person_boxes = []  # List to store bounding boxes of detected persons
-            shooting_zone_colors = {}  # Dictionary to store the color of each shooting zone
-
+            # Check if the ball is in the shooting zone of any player
+            self.current_shooting_team = None
             for track in tracks:
                 if not track.is_confirmed():
                     continue
-                track_id = track.track_id
-                bbox = track.to_ltrb()
-                x1, y1, x2, y2 = map(int, bbox)
-                cx, cy = (x1 + x2) // 2, (y1 + y2) // 2  # Center of the box
-
-                # Draw the bounding box
-                cv2.rectangle(self.frame, (x1, y1), (x2, y2), self.team_colors[track_id], 2)
-                cv2.putText(self.frame, f"ID: {track_id}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, self.team_colors[track_id], 2)
                 
-                centroids.append((cx, cy))
-                person_boxes.append((x1, y1, x2, y2))
-
-            #self.update_centroids(centroids)
-
-            '''
-            # Check if the ball is in the shooting zone of any player
-            self.current_shooting_team = None
-            for idx, (x1, y1, x2, y2) in enumerate(person_boxes):
-                shooting_zone_height = (y2 - y1) // 3
-                shooting_zone = (x1, y1, x2, y1 + shooting_zone_height)
+                track_id = track.track_id
+                x1, y1, x2, y2 = track.to_ltwh()  # Get the bounding box
+                shooting_zone_height = y2 // 3
+                shooting_zone = (x1, y1, x1 + x2, y1 + shooting_zone_height)
 
                 # Default color for the shooting zone
                 shooting_zone_color = (0, 255, 0)  # Green
@@ -119,14 +101,19 @@ class Shot:
                 # Check if the ball is in the shooting zone
                 if ball_position and shooting_zone[0] < ball_position[0] < shooting_zone[2] and shooting_zone[1] < ball_position[1] < shooting_zone[3]:
                     shooting_zone_color = (0, 0, 255)  # Red
-                    self.current_shooting_team = self.team_colors[self.get_closest_centroid(centroids, ball_position)]
+                    self.current_shooting_team = self.team_colors[track_id]
                     self.last_shooting_team = self.current_shooting_team
 
-                shooting_zone_colors[idx] = shooting_zone_color
+                # Assign team colors
+                color = self.team_colors[track_id]
+                if track_id in self.team2_ids:
+                    color = (0, 165, 255)  # Orange for team 2
 
-                # Draw the shooting zone for each player
-                cv2.rectangle(self.frame, (shooting_zone[0], shooting_zone[1]), (shooting_zone[2], shooting_zone[3]), shooting_zone_color, 2)
-            '''
+                # Draw the shooting zone and the bounding box for each player
+                cv2.rectangle(self.frame, (int(shooting_zone[0]), int(shooting_zone[1])), (int(shooting_zone[2]), int(shooting_zone[3])), shooting_zone_color, 2)
+                cv2.rectangle(self.frame, (int(x1), int(y1)), (int(x1 + x2), int(y1 + y2)), color, 2)
+                #cv2.putText(self.frame, f"ID: {track_id}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
             if rim_position:
                 # Define the top and bottom boxes relative to the rim position
                 rim_x, rim_y = rim_position
@@ -155,15 +142,14 @@ class Shot:
             cv2.putText(self.frame, "Orange: ", (10, 470), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 165, 255), 2)
             cv2.putText(self.frame, f"{self.num_orange_buckets}", (150, 470), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
             cv2.putText(self.frame, "| Blue: ", (200, 470), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-            cv2.putText(self.frame, f"{self.num_blue_buckets}", (310, 470), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+            cv2.putText(self.frame, f"{self.num_blue_buckets}", (350, 470), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
 
-            '''
             # Display the shooting team permanently
             if self.last_shooting_team == (0, 165, 255):  # Orange
                 cv2.putText(self.frame, "Shooting: Orange", (frame_width - 250, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 165, 255), 2)
             elif self.last_shooting_team == (255, 0, 0):  # Blue
                 cv2.putText(self.frame, "Shooting: Blue", (frame_width - 250, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-            '''
+
             # Check if the ball is above the rim and manage the dots
             if ball_position and rim_position and ball_position[1] < rim_y:
                 if not self.dots or time.time() - self.dots[-1]['time'] >= 0.1:
@@ -195,38 +181,20 @@ class Shot:
 
     def on_mouse_click(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
-            # Check if the click is near any centroid
-            for object_id, centroid in self.centroids.items():
-                cx, cy = centroid
+            # Check if the click is near any tracked object
+            for track in self.tracker.tracker.tracks:
+                if not track.is_confirmed():
+                    continue
+                x1, y1, x2, y2 = track.to_ltwh()
+                cx, cy = x1 + x2 // 2, y1 + y2 // 2
                 if abs(cx - x) < 10 and abs(cy - y) < 10:
-                    # Assign this centroid to team 2
-                    if centroid not in self.team2_centroids:
-                        self.team2_centroids.append(centroid)
-                        self.team_colors[object_id] = (0, 165, 255)  # Orange
+                    # Assign this track ID to team 2
+                    if track.track_id not in self.team2_ids:
+                        self.team2_ids.add(track.track_id)
+                        self.team_colors[track.track_id] = (0, 165, 255)  # Orange
                     else:
-                        self.team2_centroids.remove(centroid)  # Allow toggling back to team 1
-                        self.team_colors[object_id] = (255, 0, 0)  # Blue
-
-    def update_centroids(self, centroids):
-        # This method updates centroids based on the given detections
-        updated_centroids = {}
-        for i, centroid in enumerate(centroids):
-            updated_centroids[i] = centroid
-        self.centroids = updated_centroids
-        return updated_centroids
-
-    def get_closest_centroid(self, centroids, ball_position):
-        closest_id = None
-        closest_distance = float('inf')
-        for object_id, centroid in centroids.items():
-            distance = self.calculate_distance(centroid, ball_position)
-            if distance < closest_distance:
-                closest_distance = distance
-                closest_id = object_id
-        return closest_id
-
-    def calculate_distance(self, point1, point2):
-        return np.linalg.norm(np.array(point1) - np.array(point2))
+                        self.team2_ids.remove(track.track_id)  # Allow toggling back to team 1
+                        self.team_colors[track.track_id] = (255, 0, 0)  # Blue
 
 if __name__ == "__main__":
     Shot()

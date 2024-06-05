013130dda297df0e8de6d72ae5e56f3b988a59a5
BenjaminNamayandev <benjaminnamayandeh@gmail.com>
2024-06-04 22:12:36 -0400

    Removed all extra files

--- old/test.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torchvision
-print("PyTorch version:", torch.__version__)
-print("torchvision version:", torchvision.__version__)
-if torch.cuda.is_available():
-    print("CUDA is available. PyTorch was compiled with CUDA support.")
-else:
-    print("CUDA is not available. PyTorch was not compiled with CUDA support.")
